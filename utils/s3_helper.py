32544b53953e40b1e423c2e90768d6d1ec6ee5a4
Ben Clarke <bclarkeuk@googlemail.com>
2025-04-14 19:35:44 +0000

    update to the s3 helper

--- utils/s3_helper.py
+++ utils/s3_helper.py
@@ -9,6 +9,23 @@ class S3Helper:
         self.client = boto3.client("s3")
 
     
+    def add_folder(self, local_folder: str, s3_folder: str):
+        """upload all files in a folder to S3"""
+        pass
+
+    def list_folder(self, s3_folder:str = None):
+        """list all of the files in a folder"""
+        if s3_folder != None:
+            folder = f'{self.username}/{self.s3_sub_folder}/{s3_folder}'
+            
+
+        files = []
+        for file in self.client.list_objects(Bucket=self.bucket, Prefix=folder)['Contents']:
+            key = file['Key']
+            files.append(key)
+        return files
+        
+    
     def clear_folder(self, local_folder, count=0):
         try:
             for root, dirs, files in os.walk(local_folder, topdown=False):
